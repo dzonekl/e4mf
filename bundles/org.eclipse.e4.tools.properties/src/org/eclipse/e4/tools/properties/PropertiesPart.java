7400a4a8f909e3dd1b0eb8b6cd98171e9ecbdd6d
Christophe Bouhier <dzonekl@gmail.com>
2013-04-12 17:11:04 +0200

    - properties bundle. - icons missing from edit.ui - add SelectAll Handler.

---
+++ bundles/org.eclipse.e4.tools.properties/src/org/eclipse/e4/tools/properties/PropertiesPart.java
@@ -0,0 +1,20 @@
+package org.eclipse.e4.tools.properties;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.ui.di.Focus;
+
+
+@SuppressWarnings("restriction")
+public class PropertiesPart {
+	@Inject
+	public PropertiesPart() {
+		// TODO Your code here
+	}
+
+	@Focus
+	public void onFocus() {
+		// TODO Your code here
+	}
+
+}
